f0d414dd33bed02e7b6be223da913e4e70684a12
Yondonjamts G <yondonjamts.g@tavanbogd.com>
2024-04-29 17:50:36 +0800

    some change

--- src/components/custom/Header.tsx
+++ src/components/custom/Header.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from 'react';
 const Header = ({ title, border = false, action }: { title?: string; border?: boolean; action?: ReactNode }) => {
    return (
       <div className={`pt-2 mb-4 flex items-center justify-between ${border ? `border-b` : ``}`}>
-         <h3 className={`text-xl pb-4 font-normal ${border ? `border-b border-primary` : ``} `}>{title}</h3>
+         <h3 className={`text-xl pb-4 font-medium ${border ? `border-b border-primary` : ``} `}>{title}</h3>
          {action}
       </div>
    );
